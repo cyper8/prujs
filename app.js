6a7625f2b6d07e1f08bfb9baa4583c2f3042853b
Anton Yashchuk <anton.yashchuk@gmail.com>
2018-08-08 22:04:33 +0300

    localStorage emulated (nonpersistent of course) if is not available

--- app.js
+++ app.js
@@ -4,7 +4,83 @@ var App;
 function main(){
 	var out = document.getElementById("output");
 
-	var store = window.localStorage;
+	var store = (function(){
+	  function storageAvailable(type) {
+	  	try {
+	  		var storage = window[type],
+	  			x = '__storage_test__';
+	  		storage.setItem(x, x);
+	  		storage.removeItem(x);
+	  		return true;
+	  	}
+	  	catch(e) {
+	  		return false;
+	  	}
+	  }
+
+		if (storageAvailable('localStorage')){
+	    return window.localStorage;
+	  }
+	  else{
+	    return Object.defineProperties(new Object(),{
+	      'setItem': {
+	        value: function(k,v){
+	          var e = new CustomEvent('storage',{detail:{
+	            key:k,
+	            oldValue:this[k],
+	            newValue:v,
+	            storageArea:this
+	          }});
+	          this[k] = v;
+	          (window || self).dispatchEvent(e);
+	          return v;
+	        }
+	      },
+	      'getItem': {
+	        value: function(k){
+	          return this[k] || null;
+	        }
+	      },
+	      'removeItem': {
+	        value: function(k){
+	          delete this[k];
+	          return this;
+	        }
+	      },
+	      'key': {
+	        value: function(n){
+	          var i,c=0;
+	          for (i in this){
+	            if (c==n) return this[i];
+	            c++;
+	          }
+	        }
+	      },
+	      'clear': {
+	        value: function(){
+	          var i;
+	          for (i in this){
+	            this.removeItem(i);
+	          }
+	          return this;
+	        }
+	      },
+	      'length': {
+	        get: function(){
+	          var i,c=0;
+	          for (i in this){
+	            c++;
+	          }
+	          return c;
+	        }
+	      },
+				'fake': {
+					value: true,
+					
+				}
+	    });
+	  }
+	})();
 
   function Debounced(func,backoff){
   	var timer;
